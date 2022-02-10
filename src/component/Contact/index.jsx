4496e84119e52384d49a852c992cb927dfd8bae3
Junya Qiao <62684140+JunyaQ@users.noreply.github.com>
2022-02-09 22:04:33 -0500

    the web information send success

--- src/component/Contact/index.jsx
+++ src/component/Contact/index.jsx
@@ -2,17 +2,43 @@ import React, { useState } from 'react';
 import Footer from '../Footer';
 import{Form, Input, Button, TextArea} from 'semantic-ui-react';
 import './style.css'
+//import 'dotenv/config'//for .env file info
+
+
+import emailjs from 'emailjs-com';
+//require('dotenv').contact();
+//console.log(process.env)
+
 
 function Contact(){
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const [errorMessage, setErrorMessage] = useState('');
-     const { name, email, message } = formState;
-    const handleSubmit = (e) => {
+    // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    //  const [errorMessage, setErrorMessage] = useState('');
+    //   const { name, email, message } = formState;
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     if (!errorMessage) {
+    //       setFormState({ [e.target.name]: e.target.value });
+    //       console.log('Form', formState);
+    //     }
+    //   };
+
+       const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+       const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+       const USER_ID = process.env.REACT_APP_USER_ID;
+       console.log(SERVICE_ID);
+       console.log(TEMPLATE_ID);
+       console.log(USER_ID);
+      const handleSubmit = (e) => {
         e.preventDefault();
-        if (!errorMessage) {
-          setFormState({ [e.target.name]: e.target.value });
-          console.log('Form', formState);
-        }
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+          .then((result) => {
+            console.log(result.text);
+            alert("successfully submited")
+          }, (error) => {
+            console.log(error.text);
+            alert("Something went wrong")
+          });
+        e.target.reset()
       };
    
  
