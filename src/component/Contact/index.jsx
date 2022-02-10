c8f163a9d590a8b259ec487594e619fad8f75946
Junya Qiao <62684140+JunyaQ@users.noreply.github.com>
2022-02-09 22:57:56 -0500

    add information to env

--- src/component/Contact/index.jsx
+++ src/component/Contact/index.jsx
@@ -2,35 +2,21 @@ import React, { useState } from 'react';
 import Footer from '../Footer';
 import{Form, Input, Button, TextArea} from 'semantic-ui-react';
 import './style.css'
-//import 'dotenv/config'//for .env file info
-
 
 import emailjs from 'emailjs-com';
-//require('dotenv').contact();
-//console.log(process.env)
-
-
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const USER_ID = process.env.REACT_APP_USER_ID;
+emailjs.init(USER_ID);
 function Contact(){
-    // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    //  const [errorMessage, setErrorMessage] = useState('');
-    //   const { name, email, message } = formState;
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     if (!errorMessage) {
-    //       setFormState({ [e.target.name]: e.target.value });
-    //       console.log('Form', formState);
-    //     }
-    //   };
-
-       const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
-       const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
-       const USER_ID = process.env.REACT_APP_USER_ID;
+      
        console.log(SERVICE_ID);
        console.log(TEMPLATE_ID);
        console.log(USER_ID);
       const handleSubmit = (e) => {
+        console.log(e);
         e.preventDefault();
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target)
           .then((result) => {
             console.log(result.text);
             alert("successfully submited")
@@ -44,27 +30,6 @@ function Contact(){
  
     return(
 <section className='formsection'>
-      {/* <h1 className='contactme'>Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div> 
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email}  />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form> */}
       <Form onSubmit={handleSubmit}>
         <Form.Field
         id='email'
